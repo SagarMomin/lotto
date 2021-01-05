be119af79dec869d0acf23b54f2c0badca144469
Sagar <sagar.momin@gmail.com>
2021-01-05 14:12:25 -0500

    Upating to a new version of dune/ocaml

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 A simple OCaml program that shows some language basics. 
 
-The program calculates how much money you've won in Mega Millions or Powerball
+The program calculates how much money you've won in Mega Millions or Powerball.
--- bin/lotto.ml
+++ bin/lotto.ml
@@ -7,10 +7,9 @@ module Common_options = struct
   let tickets_file =
     flag
       "ticket-file"
-      (required file)
+      (required Filename.arg_type)
       ~doc:
-        "[FILE] A csv like file that has contains the all the numbers: in the \
-         form of\n\n\
+        "[FILE] A csv like file that has contains the all the numbers: in the form of\n\n\
         \        num,num,num,num,num,num\n\n\
         \        num,num,num,num,num,num"
   ;;
@@ -20,8 +19,8 @@ module Common_options = struct
       "winning-number"
       (required string)
       ~doc:
-        "[STRING] Winning number as a string in the form of:\n\n     \
-         num,num,num,num,num,num"
+        "[STRING] Winning number as a string in the form of:\n\n\
+        \     num,num,num,num,num,num"
   ;;
 end
 
@@ -33,17 +32,15 @@ module Lottery = struct
       }
 
     let t_of_int_list l =
-      { white_balls = List.slice l 0 (-1) |> Int.Set.of_list
-      ; magic = List.last_exn l
-      }
+      { white_balls = List.slice l 0 (-1) |> Int.Set.of_list; magic = List.last_exn l }
     ;;
 
     let to_string { white_balls; magic } =
       sprintf
         "%s,%d"
         (Set.to_list white_balls
-         |> List.map ~f:Int.to_string_hum
-         |> String.concat ~sep:",")
+        |> List.map ~f:Int.to_string_hum
+        |> String.concat ~sep:",")
         magic
     ;;
 
@@ -51,22 +48,18 @@ module Lottery = struct
       In_channel.read_lines ticket_file
       |> List.map ~f:(String.split ~on:',')
       |> List.filter_map ~f:(fun ticket ->
-          if List.length ticket <> 6
-          then (
-            printf "Invalid ticket %s\n:" @@ String.concat ~sep:"," ticket;
-            None)
-          else Some (List.map ticket ~f:Int.of_string))
+             if List.length ticket <> 6
+             then (
+               printf "Invalid ticket %s\n:" @@ String.concat ~sep:"," ticket;
+               None)
+             else Some (List.map ticket ~f:Int.of_string))
       |> List.map ~f:t_of_int_list
     ;;
 
     let see_what_matches ~(winning_ticket : t) (t : t) =
       let { white_balls; magic } = t in
-      let { white_balls = winning_white_balls; magic = winning_magic } =
-        winning_ticket
-      in
-      let num_match =
-        Set.inter white_balls winning_white_balls |> Set.length
-      in
+      let { white_balls = winning_white_balls; magic = winning_magic } = winning_ticket in
+      let num_match = Set.inter white_balls winning_white_balls |> Set.length in
       let does_magic_match = magic = winning_magic in
       t, num_match, does_magic_match
     ;;
@@ -81,28 +74,28 @@ module Lottery = struct
       match t with
       | Megamillions ->
         (match magic_ball, white_balls_matched with
-         | true, 5 -> 99999999999
-         | true, 4 -> 10_000
-         | true, 3 -> 200
-         | true, 2 -> 10
-         | true, 1 -> 4
-         | true, 0 -> 2
-         | false, 5 -> 1_000_000
-         | false, 4 -> 500
-         | false, 3 -> 10
-         | _ -> 0)
+        | true, 5 -> 99999999999
+        | true, 4 -> 10_000
+        | true, 3 -> 200
+        | true, 2 -> 10
+        | true, 1 -> 4
+        | true, 0 -> 2
+        | false, 5 -> 1_000_000
+        | false, 4 -> 500
+        | false, 3 -> 10
+        | _ -> 0)
       | Powerball ->
         (match magic_ball, white_balls_matched with
-         | true, 5 -> 99999999999
-         | true, 4 -> 50_000
-         | true, 3 -> 100
-         | true, 2 -> 7
-         | true, 1 -> 4
-         | true, 0 -> 4
-         | false, 5 -> 1_000_000
-         | false, 4 -> 100
-         | false, 3 -> 7
-         | _ -> 0)
+        | true, 5 -> 99999999999
+        | true, 4 -> 50_000
+        | true, 3 -> 100
+        | true, 2 -> 7
+        | true, 1 -> 4
+        | true, 0 -> 4
+        | false, 5 -> 1_000_000
+        | false, 4 -> 100
+        | false, 3 -> 7
+        | _ -> 0)
     in
     if winnings > 0 then Some (ticket, winnings) else None
   ;;
@@ -136,19 +129,15 @@ let lotto ~lottery =
   Command.async
     ~summary:"Check lotto tickets and see if there is a winner"
     (let open Command.Let_syntax in
-     let%map_open ticket_file = Common_options.tickets_file
-     and winning_ticket = Common_options.winning_ticket in
-     fun () ->
-       check_tickets ~lottery ~ticket_file ~winning_ticket;
-       Deferred.unit)
+    let%map_open ticket_file = Common_options.tickets_file
+    and winning_ticket = Common_options.winning_ticket in
+    fun () ->
+      check_tickets ~lottery ~ticket_file ~winning_ticket;
+      Deferred.unit)
 ;;
 
 let command =
   Command.group
     ~summary:"Check to see how much money was won in the lotery"
-    [ "megamillions", lotto ~lottery:Megamillions
-    ; "powerball", lotto ~lottery:Powerball
-    ]
+    [ "megamillions", lotto ~lottery:Megamillions; "powerball", lotto ~lottery:Powerball ]
 ;;
-
-let () = Command.run command
